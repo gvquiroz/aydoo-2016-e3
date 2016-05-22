338115078ef513f7318ceacb546c687965489b23
Gonza <g.d.escobar88@gmail.com>
2016-05-22 03:06:41 -0300

    se crea texto plano y la salida es igual a la entrada

--- src/test/java/ar/edu/untref/aydoo/TextoTest.java
+++ src/test/java/ar/edu/untref/aydoo/TextoTest.java
@@ -5,13 +5,21 @@ import org.junit.Test;
 
 public class TextoTest {
 
-    @Test
-    public void muestraTituloEntrante(){
+	@Test
+	public void muestraTituloEntrante() {
 
-        Texto texto = new Texto("Buenos Dias");
-        String textoEntrada = texto.getEntrada();
+		Texto texto = new Texto("Buenos Dias");
+		String textoEntrada = texto.getEntrada();
 
-        Assert.assertEquals("Buenos Dias", textoEntrada);
-    }
+		Assert.assertEquals("Buenos Dias", textoEntrada);
+	}
 
+	@Test
+	public void muestralTextoTransformadoSalida() {
+
+		Texto unTitulo = new Texto("Buenos Dias");
+		String textoSalida = unTitulo.getSalida();
+
+		Assert.assertEquals("Buenos Dias", textoSalida);
+	}
 }
