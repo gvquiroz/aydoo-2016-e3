a04154d6d8a5e5aad372fd824b68477e2123e048
ldelrio1 <lucasdr.lgi@gmail.com>
2016-05-24 00:43:59 -0300

    Agrega Tests Analisis de varios elementos

--- src/test/java/ar/edu/untref/aydoo/AnalizadorDeContenidoTest.java
+++ src/test/java/ar/edu/untref/aydoo/AnalizadorDeContenidoTest.java
@@ -71,4 +71,50 @@ public class AnalizadorDeContenidoTest {
 
     }
 
+    @Test
+    public void recibeVariosElementosVerificaTitulo(){
+
+        String contenido = "# unTitulo\n## unSubTitulo\ni:imagen.png\n*unItem\n" +
+                "*otroItem\n" +
+                "*tercerItem\n";
+
+        AnalizadorDeContenido analizador = new AnalizadorDeContenido(contenido);
+        analizador.analizarContenido();
+        elementosContenido = analizador.obtenerContenidoAnalizado();
+        String titulo = "# unTitulo\n";
+
+        Assert.assertEquals(titulo, elementosContenido.get(0).getEntrada());
+
+    }
+
+    @Test
+    public void recibeVariosElementosVerificaSubtitulo(){
+
+        String contenido = "# unTitulo\n## unSubTitulo\ni:imagen.png\n*unItem\n" +
+                "*otroItem\n" +
+                "*tercerItem\n";
+
+        AnalizadorDeContenido analizador = new AnalizadorDeContenido(contenido);
+        analizador.analizarContenido();
+        elementosContenido = analizador.obtenerContenidoAnalizado();
+        String subTitulo = "## unSubTitulo\n";
+
+        Assert.assertEquals(subTitulo, elementosContenido.get(1).getEntrada());
+
+    }
+
+    @Test
+    public void recibeVariosElementosVerificaElUltimo(){
+
+        String contenido = "# unTitulo\n## unSubTitulo\ni:imagen.png\n*unItem\n" +
+                "*otroItem\n";
+
+        AnalizadorDeContenido analizador = new AnalizadorDeContenido(contenido);
+        analizador.analizarContenido();
+        elementosContenido = analizador.obtenerContenidoAnalizado();
+        String lista = "*unItem\n*otroItem\n";
+
+        Assert.assertEquals(lista, elementosContenido.get(3).getEntrada());
+
+    }
 }
