158b28bbffdf38c83ef8147e76e83fbf56c89043
gvquiroz <gvquiroz@gmail.com>
2016-05-25 23:33:16 -0300

    Agregada primera parte a Esqueleto LectorDeMarkdown

--- src/main/java/ar/edu/untref/aydoo/LectorDeMarkdown.java
+++ src/main/java/ar/edu/untref/aydoo/LectorDeMarkdown.java
@@ -12,6 +12,16 @@ public class LectorDeMarkdown {
 	public void lectorMarkDown(File archivo) throws FileNotFoundException, IOException {
 		/*
 		//TODO: Acá ponemos la magia.
+
+		//Creamos las opciones validas, me falta liquidar el builder de opciones y se las pasamos al procesadorDeArgumentos
+		ProcesadorDeArgumentos miProcesador = new ProcesadorDeArgumentos(argumentos,opcionesValidas);
+
+		// Se le pide al precesador el valor de la opcion con el nombre de la opcion agregada
+		String modo = miProcesador.getContenido("--modo");
+		String nombreDeArchivo = miProcesador.getContenido("sin nombre");
+		//Pasa a validar el nombre del archivo
+		ValidadorDeNombreDeArchivo = new ValidadorDeNombreDeArchivo(nombreDeArchivo)
+
 		GestorDeArchivos gestorDeArchivos = new GestorDeArchivos();
 		//leer archivos retorna el contenido del archivo markdown en un solo string resultado
 		String resultado = gestorDeArchivos.leerArchivo(archivo);
