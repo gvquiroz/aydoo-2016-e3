05eb34dbef21f6e353cf2224cc5741a71b2ae06d
Gonza <g.d.escobar88@gmail.com>
2016-05-27 20:48:23 -0300

    refactor excepciones

--- src/main/java/ar/edu/untref/aydoo/ArgumentoInvalidoException.java
+++ src/main/java/ar/edu/untref/aydoo/ArgumentoInvalidoException.java
@@ -4,4 +4,11 @@ package ar.edu.untref.aydoo;
  * Created by Velonter on 5/23/2016.
  */
 public class ArgumentoInvalidoException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	private final static String mensaje = "Los parametros ingresados no son validos.";
+
+	public ArgumentoInvalidoException() {
+		super(mensaje);
+	}
 }
--- src/main/java/ar/edu/untref/aydoo/NombreDeArchivoIncorrectoException.java
+++ src/main/java/ar/edu/untref/aydoo/NombreDeArchivoIncorrectoException.java
@@ -4,4 +4,13 @@ package ar.edu.untref.aydoo;
  * Created by Velonter on 5/20/2016.
  */
 public class NombreDeArchivoIncorrectoException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	private final static String mensaje = "El nombre del archivo ingresado no es valido.";
+
+	public NombreDeArchivoIncorrectoException() {
+		super(mensaje);
+	}
+	
 }
