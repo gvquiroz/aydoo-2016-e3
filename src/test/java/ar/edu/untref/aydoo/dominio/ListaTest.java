c8b26e26502fed9303cd5e5f7e5741195d258d5a
gvquiroz <gvquiroz@gmail.com>
2016-06-21 00:09:44 -0300

    Se agrega test lista con tres items

--- src/test/java/ar/edu/untref/aydoo/dominio/ListaTest.java
+++ src/test/java/ar/edu/untref/aydoo/dominio/ListaTest.java
@@ -27,6 +27,16 @@ public class ListaTest {
 	    "<ul>\n<li>hola</li>\n<li>hola</li>\n</ul>\n" +
 	    "</section>\n", resultado);
 	 }
+	@Test
+	public void analizadoConListaConTresItems() {
+		String entradaMD = "--- \n*hola\n*hola\n*hola\n";
+		AnalizadorDeContenido analizador = new AnalizadorDeContenido(entradaMD);
+		String resultado = analizador.analizarContenido();
+
+		Assert.assertEquals("<section>\n" +
+				"<ul>\n<li>hola</li>\n<li>hola</li>\n<li>hola</li>\n</ul>\n" +
+				"</section>\n", resultado);
+	}
 
 	 @Test
 	 public void analizadoConListaConDosItemsYUnTituloEntreMedio() {
